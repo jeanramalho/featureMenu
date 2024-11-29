878c416872951f1cdb413ba7b1979a431ad9f64c
jeanramalho.dev <jeanramalho.dev@gmail.com>
2024-11-29 12:39:50 -0300

    [add] - Cria mock para integração mockada com aplicativo

--- featureMenu/SceneDelegate.swift
+++ featureMenu/SceneDelegate.swift
@@ -16,7 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
      
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
-        let vc: ViewController = ViewController()
+        let vc: menuProfileVC = menuProfileVC()
         let navVc = UINavigationController(rootViewController: vc)
         window.rootViewController = navVc
         window.makeKeyAndVisible()
---
+++ featureMenu/ViewModel/MenuProfileViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  MenuProfileViewModel.swift
+//  featureMenu
+//
+//  Created by Jean Ramalho on 29/11/24.
+//
+
+import UIKit
+
+class MenuProfileViewModel {
+
+}
