bfb3e9a260ccfda676970c383bae43af712bc362
jeanramalho.dev <jeanramalho.dev@gmail.com>
2024-12-18 12:01:21 -0300

    [add] - Cria elemento de header para screen

--- featureMenu/featureMenu/Screen/MenuProfileScreen.swift
+++ featureMenu/featureMenu/Screen/MenuProfileScreen.swift
@@ -8,13 +8,32 @@
 import UIKit
 
 class MenuProfileScreen: UIView {
+    
+    lazy var headerView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .red
+        return view
+    }()
 
     override init(frame: CGRect) {
         super.init(frame: frame)
+        self.backgroundColor = .purple
+        self.addSubview(self.headerView)
+        self.setConstraints()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setConstraints(){
+        NSLayoutConstraint.activate([
+            self.headerView.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor),
+            self.headerView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            self.headerView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            self.headerView.heightAnchor.constraint(equalToConstant: 40)
+        ])
     }
     
 }
--- featureMenu/featureMenu/VC/MenuProfileVC.swift
+++ featureMenu/featureMenu/VC/MenuProfileVC.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class MenuProfileVC: UIViewController {
     
+    var screen: MenuProfileScreen?
+    
     let viewModel: MenuProfileViewModel = MenuProfileViewModel()
+    
+    override func loadView() {
+        self.screen = MenuProfileScreen()
+        self.view = self.screen
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.isNavigationBarHidden = true
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .red
         viewModel.delegate(delegate: self)
         viewModel.fetch(.mock)
     }
