d6cfc9d3e894bf054c0514477265a9beb95d131a
jeanramalho.dev <jeanramalho.dev@gmail.com>
2024-12-18 11:40:35 -0300

    [add] - Inicia a criação do header da View principal

---
+++ featureMenu/featureMenu/Screen/MenuProfileScreen.swift
@@ -0,0 +1,20 @@
+//
+//  MenuProfileScreen.swift
+//  featureMenu
+//
+//  Created by Jean Ramalho on 18/12/24.
+//
+
+import UIKit
+
+class MenuProfileScreen: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
