540c95e57157a1704c1702bc5c3b8b60a6eb7cef
jeanramalho.dev <jeanramalho.dev@gmail.com>
2024-12-22 18:41:07 -0300

    [add] - Cria as configurações iniciais da tableView

--- featureMenu/featureMenu/VC/MenuProfileVC.swift
+++ featureMenu/featureMenu/VC/MenuProfileVC.swift
@@ -32,14 +32,36 @@ class MenuProfileVC: UIViewController {
 
 extension MenuProfileVC: UITableViewDelegate, UITableViewDataSource {
     
-    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 0
     }
     
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        return nil
+    }
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 60
+    }
+    
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return .leastNormalMagnitude
+    }
+    
+    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        return nil
     }
     
     
