dee7402b9334fbe71bef3cd5889853f2275d00cc
jeanramalho.dev <jeanramalho.dev@gmail.com>
2024-12-24 10:35:41 -0300

    [add] - Finaliza criação de elementos para a celula

--- featureMenu/featureMenu/Cell/MenuProfileTableViewCell.swift
+++ featureMenu/featureMenu/Cell/MenuProfileTableViewCell.swift
@@ -8,10 +8,34 @@
 import UIKit
 
 class MenuProfileTableViewCell: UITableViewCell {
+    
+    static let identifier: String = "MenuProfileTableViewCell"
+    
+    let screen: MenuProfileTableViewCellScreen = MenuProfileTableViewCellScreen()
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        self.selectionStyle = .none
+        self.screen.translatesAutoresizingMaskIntoConstraints = false
+        self.contentView.addSubview(screen)
+        self.configConstraint()
     }
-
+    
+    private func configConstraint(){
+        NSLayoutConstraint.activate([
+            screen.topAnchor.constraint(equalTo: self.topAnchor),
+            screen.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            screen.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            screen.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+        ])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    public func setupCell(title: String){
+        screen.titleLabel.text = title
+    }
+    
 }
--- featureMenu/featureMenu/Cell/Screen/MenuProfileTableViewCellScreen.swift
+++ featureMenu/featureMenu/Cell/Screen/MenuProfileTableViewCellScreen.swift
@@ -8,13 +8,45 @@
 import UIKit
 
 class MenuProfileTableViewCellScreen: UIView {
+    
+    lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .black
+        label.font = UIFont.boldSystemFont(ofSize: 14)
+        return label
+    }()
+    
+    lazy var lineView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .lightGray
+        return view
+    }()
 
     override init(frame: CGRect) {
         super.init(frame: frame)
+        backgroundColor = .lightGray.withAlphaComponent(0.1)
+        addSubview(titleLabel)
+        addSubview(lineView)
+        configConstraint()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configConstraint(){
+        NSLayoutConstraint.activate([
+            titleLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            titleLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 25),
+            titleLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -10),
+            
+            lineView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            lineView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            lineView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+            lineView.heightAnchor.constraint(equalToConstant: 0.8)
+        ])
     }
     
 }
