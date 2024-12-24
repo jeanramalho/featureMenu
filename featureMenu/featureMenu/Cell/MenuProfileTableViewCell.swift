46532a2406ccfdd50d393326302d0a35c37bfe9e
jeanramalho.dev <jeanramalho.dev@gmail.com>
2024-12-24 10:12:54 -0300

    [add] - Inicia a criação da célula da tableView

---
+++ featureMenu/featureMenu/Cell/MenuProfileTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  MenuProfileTableViewCell.swift
+//  featureMenu
+//
+//  Created by Jean Ramalho on 24/12/24.
+//
+
+import UIKit
+
+class MenuProfileTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ featureMenu/featureMenu/Cell/Screen/MenuProfileTableViewCellScreen.swift
@@ -0,0 +1,20 @@
+//
+//  MenuProfileTableViewCellScreen.swift
+//  featureMenu
+//
+//  Created by Jean Ramalho on 24/12/24.
+//
+
+import UIKit
+
+class MenuProfileTableViewCellScreen: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
